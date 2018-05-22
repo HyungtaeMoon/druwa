264de042de0af67bdf09b321bb8f9cb80bdec3b3
Hyungtae Moon <blessmht@gmail.com>
2018-05-22 13:29:36 +0900

    mission fail

--- python/problem01.py
+++ python/problem01.py
@@ -1,3 +1,21 @@
 """
 매개변수로 문자열을 받고, 해당 문자열이 red면 apple을, yellow면 banana를, green이면 melon을, 어떤 경우도 아닐 경우 I don't know를 리턴하는 함수를 정의하고, 사용하여 result변수에 결과를 할당하고 print해본다.
 """
+def result(*args):
+    args = input("input your favortie color: ")
+
+    if args == 'red':
+        print('apple')
+
+    elif args == 'yellow':
+        print('banana')
+
+    elif args == 'green':
+        print('melon')
+
+    else:
+        print('I don\'t know')
+
+'''
+에러메시지 출력
+'''
--- python/problem02.py
+++ python/problem02.py
@@ -1,3 +1,20 @@
 """
 problem01에서 작성한 함수에 docstring을 작성하여 함수에 대한 설명을 달아보고, help(함수명)으로 해당 설명을 출력해본다.
 """
+def result(*args):
+
+    args = input("input your favorite color: ")
+    
+    if args == 'red':
+        print('apple')
+    
+    elif args == 'yellow':
+        print('banana')
+    
+    elif args == 'green':
+        print('melon')
+    
+    else:
+        print('I don\'t know')
+
+
